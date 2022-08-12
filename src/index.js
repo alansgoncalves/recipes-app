730631f5b599272c47bca76f0910bca8f2bfdf71
alansgoncalves <vekio.soft@gmail.com>
2022-08-12 10:38:14 -0300

    fix: add basename

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import './styles/Global.css';
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "./styles/Global.css";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/recipes-app">
     <App />
   </BrowserRouter>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
